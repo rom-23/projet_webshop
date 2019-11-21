e8b1cd0110e587d24c45032c84ec91a8828ff6f8
rom <romain.laurent23@gmail.com>
2019-11-21 22:49:59 +0100

    fixtures_best

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -3,7 +3,10 @@
 namespace App\DataFixtures;
 
 use App\Entity\Category;
+use App\Entity\Comment;
+use App\Entity\Image;
 use App\Entity\Product;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
@@ -22,16 +25,50 @@ class ProductFixtures extends Fixture
             // fake pour produits
             for ($j = 1; $j <= mt_rand( 4, 6 ); $j++) {
                 $product = new Product();
-                $content = '<p>' . join( $faker -> paragraphs( 1 ), '</p><p>' ) . '</p>';
                 $product
                     -> setName( $faker -> sentence() )
-                    -> setDescription( $content )
+                    -> setDescription( $faker -> sentence() )
                     -> setCategory( $category )
                     -> setImage( $faker -> imageUrl() )
                     -> setCreatedAt( $faker -> dateTimeBetween( '-6 months' ) );
                 $manager -> persist( $product );
+                // image
+                for ($t = 1; $t <= mt_rand( 4, 10 ); $t++) {
+                    $image = new Image();
+                    $image
+                        -> setProducts( $product)
+                        -> setFileName( $faker -> imageUrl() )
+                        -> setCreatedAt( $faker -> dateTimeBetween( '-6 months' ) );
+                    $manager -> persist( $image );
+                }
+                    //  users
+                    for ($f = 1; $f <= mt_rand( 4, 8 ); $f++) {
+                        $user = new User();
+                        $user
+                            -> setUsername( $faker -> name )
+                            -> setEmail( $faker -> email )
+                            -> setPassword( 'pass-' . $f );
+                        $manager -> persist( $user );
+                        for ($k = 1; $k <= mt_rand( 4, 10 ); $k++) {
+                            $comment = new Comment();
+                            $content = '<p>' . join( $faker -> paragraphs( 1 ), '</p><p>' ) . '</p>';
+                            $now = new \DateTime();
+                            $interval = $now -> diff( $product -> getCreatedAt() );
+                            $days = $interval -> days;
+                            $minimum = '-' . $days . 'days'; // -100 days
+                            $comment
+                                -> setProducts( $product )
+                                -> setAuthor( $faker -> name )
+                                -> setContent( $content )
+                                -> setCreatedAt( $faker -> dateTimeBetween( $minimum ) )
+                                -> setUser( $user );
+                            $manager -> persist( $comment );
+                        }
+                    }
+                }
             }
+            $manager -> flush();
         }
-        $manager -> flush();
+
+
     }
-}
